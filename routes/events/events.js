1e3a76dc41c5f6c820960ebf9a4732cc7d0e9fb2
Nassim <nassim.morouche1@gmail.com>
2019-06-07 17:14:03 +0200

    added event patch on /:id

--- routes/events/events.js
+++ routes/events/events.js
@@ -16,12 +16,15 @@ router.get('/', verifyToken, async function (req, res, next) {
         await client.connect();
         const db = client.db(dbName);
         const col = db.collection('events');
-        switch(req.query.sub_only){
-            case "true": result = await col.find({sub_only: true}).toArray();
+        switch (req.query.sub_only) {
+            case "true":
+                result = await col.find({sub_only: true}).toArray();
                 break;
-            case "false": result = await col.find({sub_only: false}).toArray();
+            case "false":
+                result = await col.find({sub_only: false}).toArray();
                 break;
-            default: result = await col.find({}).toArray();
+            default:
+                result = await col.find({}).toArray();
                 break;
         }
         res.send({
@@ -76,6 +79,59 @@ router.put('/', verifyTokenAdmin, async function (req, res, next) {
         res.send(err);
     }
     client.close();
+});
+
+router.patch('/:id', verifyTokenAdmin, async function (req, res, next) {
+    const client = new MongoClient(MONGODB_URI, {useNewUrlParser: true});
+    try {
+        await client.connect();
+        const db = client.db(dbName);
+        const col = db.collection('events');
+
+        const name = req.body.name;
+        const description = req.body.description;
+        const address = req.body.address;
+        const date = req.body.date;
+        const imageURL = req.body.imageURL;
+        const price = req.body.price;
+        const sub_only = req.body.sub_only;
+        const updatedAt = dateNow();
+        let eventResult = await col.find().toArray();
+        let resultForEach = 0;
+        let event;
+
+        eventResult.forEach((resForEach) => {
+            if (resForEach._id.equals(req.params.id)) {
+                resultForEach = 1;
+                event = resForEach;
+            }
+        });
+        if (resultForEach === 0) {
+            res.status(404).send({error: 'L\'event n\'existe pas'});
+        } else {
+            let insertResult = await col.updateOne(
+                {_id: ObjectId(req.params.id)},
+                {
+                    $set: {
+                        name,
+                        description,
+                        address,
+                        date,
+                        imageURL,
+                        price,
+                        sub_only,
+                        updatedAt
+                    }
+                });
+            let newEvent = await col.find({ _id: ObjectId(req.params.id) }).toArray();
+            res.send({
+                newEvent,
+                error: null
+            });
+        }
+    } catch (err) {
+        res.send({error: err});
+    }
 });
 
 module.exports = router;
