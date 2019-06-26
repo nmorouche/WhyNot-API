cb78d122e9caade9bbfd06609c74b5f33e1e71d7
Arthur BLANC <ablanc@vente-privee.com>
2019-06-26 16:08:18 +0200

    inscription

--- routes/events/events.js
+++ routes/events/events.js
@@ -1,172 +1,210 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-const {MongoClient} = require('../../config');
-const {MONGODB_URI} = require('../../config');
-const {dbName} = require('../../config');
+const { MongoClient } = require("../../config");
+const { MONGODB_URI } = require("../../config");
+const { dbName } = require("../../config");
 
-const {verifyTokenAdmin} = require('../../middleware.js');
-const {verifyToken} = require('../../middleware.js');
-const {ObjectId} = require('../../config.js');
-const {dateNow} = require('../../config');
-const {upload} = require('../../config');
+const { verifyTokenAdmin } = require("../../middleware.js");
+const { verifyToken } = require("../../middleware.js");
+const { ObjectId } = require("../../config.js");
+const { dateNow } = require("../../config");
+const { upload } = require("../../config");
 
+// get all
+router.get("/", verifyToken, async (req, res, next) => {
+  let result;
 
-router.get('/', verifyToken, async (req, res, next) => {
-    let result;
+  const client = new MongoClient(MONGODB_URI, { useNewUrlParser: true });
+  try {
+    await client.connect();
+    const db = client.db(dbName);
+    const col = db.collection("events");
+    switch (req.query.sub_only) {
+      case "true":
+        result = await col.find({ sub_only: true }).toArray();
+        break;
+      case "false":
+        result = await col.find({ sub_only: false }).toArray();
+        break;
+      default:
+        result = await col.find({}).toArray();
+        break;
+    }
+    res.send({
+      events: result,
+      error: null
+    });
+  } catch (err) {
+    res.send(err);
+  }
+  client.close();
+});
 
-    const client = new MongoClient(MONGODB_URI, {useNewUrlParser: true});
+//get one
+router.get("/:id", verifyToken, async (req, res, next) => {
+  const client = new MongoClient(MONGODB_URI, { useNewUrlParser: true });
+  try {
+    await client.connect();
+    const db = client.db(dbName);
+    const col = db.collection("events");
+    let result = await col.find({ _id: ObjectId(req.params.id) }).toArray();
+    res.send({
+      event: result,
+      error: null
+    });
+  } catch (err) {
+    res.send(err);
+  }
+  client.close();
+});
+//ajout
+router.put(
+  "/",
+  verifyTokenAdmin,
+  upload.single("image"),
+  async (req, res, next) => {
+    const client = new MongoClient(MONGODB_URI, { useNewUrlParser: true });
     try {
-        await client.connect();
-        const db = client.db(dbName);
-        const col = db.collection('events');
-        switch (req.query.sub_only) {
-            case "true":
-                result = await col.find({sub_only: true}).toArray();
-                break;
-            case "false":
-                result = await col.find({sub_only: false}).toArray();
-                break;
-            default:
-                result = await col.find({}).toArray();
-                break;
-        }
-        res.send({
-            events: result,
-            error: null
-
-        });
+      await client.connect();
+      const db = client.db(dbName);
+      const col = db.collection("events");
+      await col.insertOne({
+        name: req.body.name,
+        description: req.body.description,
+        address: req.body.address,
+        date: req.body.date,
+        imageURL: "http://localhost:3000/" + req.file.path,
+        price: parseInt(req.body.price),
+        sub_only: JSON.parse(req.body.sub_only),
+        createdBy: req.token._id,
+        createdAt: dateNow(),
+        updatedAt: null
+      });
+      res.send({
+        error: null
+      });
     } catch (err) {
-        res.send(err);
+      res.send(err);
     }
     client.close();
-});
+  }
+);
+//modification
+router.patch("/:id", verifyTokenAdmin, async (req, res, next) => {
+  const client = new MongoClient(MONGODB_URI, { useNewUrlParser: true });
+  try {
+    await client.connect();
+    const db = client.db(dbName);
+    const col = db.collection("events");
 
+    const name = req.body.name;
+    const description = req.body.description;
+    const address = req.body.address;
+    const date = req.body.date;
+    const imageURL = req.body.imageURL;
+    const price = req.body.price;
+    const sub_only = req.body.sub_only;
+    const updatedAt = dateNow();
+    let eventResult = await col.find().toArray();
+    let resultForEach = 0;
+    let event;
 
-router.get('/:id', verifyToken, async (req, res, next) => {
-    const client = new MongoClient(MONGODB_URI, {useNewUrlParser: true});
-    try {
-        await client.connect();
-        const db = client.db(dbName);
-        const col = db.collection('events');
-        let result = await col.find({_id: ObjectId(req.params.id)}).toArray();
-        res.send({
-            event: result,
-            error: null
-        });
-    } catch (err) {
-        res.send(err);
-    }
-    client.close();
-});
-
-router.put('/', verifyTokenAdmin, upload.single('image'), async (req, res, next) => {
-    const client = new MongoClient(MONGODB_URI, {useNewUrlParser: true});
-    try {
-        await client.connect();
-        const db = client.db(dbName);
-        const col = db.collection('events');
-        await col.insertOne({
-            name: req.body.name,
-            description: req.body.description,
-            address: req.body.address,
-            date: req.body.date,
-            imageURL: "http://localhost:3000/" + req.file.path,
-            price: parseInt(req.body.price),
-            sub_only: JSON.parse(req.body.sub_only),
-            createdBy: req.token._id,
-            createdAt: dateNow(),
-            updatedAt: null
-        });
-        res.send({
-            error: null
-        });
-    } catch (err) {
-        res.send(err);
-    }
-    client.close();
-});
-
-router.patch('/:id', verifyTokenAdmin, async (req, res, next) => {
-    const client = new MongoClient(MONGODB_URI, {useNewUrlParser: true});
-    try {
-        await client.connect();
-        const db = client.db(dbName);
-        const col = db.collection('events');
-
-        const name = req.body.name;
-        const description = req.body.description;
-        const address = req.body.address;
-        const date = req.body.date;
-        const imageURL = req.body.imageURL;
-        const price = req.body.price;
-        const sub_only = req.body.sub_only;
-        const updatedAt = dateNow();
-        let eventResult = await col.find().toArray();
-        let resultForEach = 0;
-        let event;
-
-        eventResult.forEach((resForEach) => {
-            if (resForEach._id.equals(req.params.id)) {
-                resultForEach = 1;
-                event = resForEach;
-            }
-        });
-        if (resultForEach === 0) {
-            res.status(404).send({error: 'L\'event n\'existe pas'});
-        } else {
-            let insertResult = await col.updateOne(
-                {_id: ObjectId(req.params.id)},
-                {
-                    $set: {
-                        name,
-                        description,
-                        address,
-                        date,
-                        imageURL,
-                        price,
-                        sub_only,
-                        updatedAt
-                    }
-                });
-            let newEvent = await col.find({_id: ObjectId(req.params.id)}).toArray();
-            res.send({
-                newEvent,
-                error: null
-            });
+    eventResult.forEach(resForEach => {
+      if (resForEach._id.equals(req.params.id)) {
+        resultForEach = 1;
+        event = resForEach;
+      }
+    });
+    if (resultForEach === 0) {
+      res.status(404).send({ error: "L'event n'existe pas" });
+    } else {
+      let insertResult = await col.updateOne(
+        { _id: ObjectId(req.params.id) },
+        {
+          $set: {
+            name,
+            description,
+            address,
+            date,
+            imageURL,
+            price,
+            sub_only,
+            updatedAt
+          }
         }
-    } catch (err) {
-        res.send({error: err});
+      );
+      let newEvent = await col.find({ _id: ObjectId(req.params.id) }).toArray();
+      res.send({
+        newEvent,
+        error: null
+      });
     }
+  } catch (err) {
+    res.send({ error: err });
+  }
+});
+//supression
+router.delete("/:id", verifyTokenAdmin, async (req, res, next) => {
+  const client = new MongoClient(MONGODB_URI, { useNewUrlParser: true });
+  try {
+    await client.connect();
+    const db = client.db(dbName);
+    const col = db.collection("events");
+    let eventResult = await col.find().toArray();
+    let resultForEach = 0;
+    let event;
+    eventResult.forEach(resForEach => {
+      if (resForEach._id.equals(req.params.id)) {
+        resultForEach = 1;
+        event = resForEach;
+      }
+    });
+    if (resultForEach === 0) {
+      res.status(404).send({ error: "L'event n'existe pas" });
+    } else {
+      await col.deleteOne({ _id: event._id });
+      res.send({
+        error: null
+      });
+    }
+  } catch (err) {
+    res.send({ error: err });
+  }
 });
 
-router.delete('/:id', verifyTokenAdmin, async (req, res, next) => {
-    const client = new MongoClient(MONGODB_URI, {useNewUrlParser: true});
-    try {
-        await client.connect();
-        const db = client.db(dbName);
-        const col = db.collection('events');
-        let eventResult = await col.find().toArray();
-        let resultForEach = 0;
-        let event;
-        eventResult.forEach((resForEach) => {
-            if (resForEach._id.equals(req.params.id)) {
-                resultForEach = 1;
-                event = resForEach;
-            }
-        });
-        if (resultForEach === 0) {
-            res.status(404).send({error: 'L\'event n\'existe pas'});
-        } else {
-            await col.deleteOne({_id: event._id});
-            res.send({
-                error: null
-            });
-        }
-    } catch(err) {
-        res.send({error: err});
-    }
-});
+//inscription
+router.patch("inscription/:id", verifyToken, async (req, res, next) => {
+  const client = new MongoClient(MONGODB_URI, { useNewUrlParser: true });
+  try {
+    await client.connect();
+    const db = client.db(dbName);
+    const colEvent = db.collection("events");
+    const colUser = db.collection("users");
+    let eventResult = await colEvent
+      .find({ _id: ObjectId(req.param.id) })
+      .toArray();
+    let resultForEach = 0;
+    let event;
+    eventResult.forEach(resForEach => {
+      if (resForEach._id.equals(req.params.id)) {
+        resultForEach = 1;
+        event = resForEach;
+      }
+    });
+    if (resultForEach === 0) {
+      res.status(404).send({ error: "L'event n'existe pas" });
+    } else {
+      //verifier que participationEvent possede une date anterieur a today ou pas de date
 
+      await colUser.updateOne(
+        { _id: ObjectId(token.id) },
+        { $set: { participationEvent: event.date } }
+      );
+    }
+  } catch (err) {
+    res.send({ error: err });
+  }
+});
 
 module.exports = router;
--- routes/users/users.js
+++ routes/users/users.js
@@ -1,132 +1,153 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-const {MongoClient} = require('../../config');
-const {MONGODB_URI} = require('../../config');
-const {JWT_KEY} = require('../../config');
-const {dbName} = require('../../config');
-const {jwt} = require('../../config');
-const {isUsernameValid} = require('../../config');
-const {md5} = require('../../config');
-const {dateNow} = require('../../config');
-const {validator} = require('../../config');
+const { MongoClient } = require("../../config");
+const { MONGODB_URI } = require("../../config");
+const { JWT_KEY } = require("../../config");
+const { dbName } = require("../../config");
+const { jwt } = require("../../config");
+const { isUsernameValid } = require("../../config");
+const { md5 } = require("../../config");
+const { dateNow } = require("../../config");
+const { validator } = require("../../config");
 
 /* GET users listing. */
-router.get('/login', async function (req, res, next) {
-    const client = new MongoClient(MONGODB_URI, {useNewUrlParser: true});
-    try {
-        await client.connect();
-        const db = client.db(dbName);
-        const col = db.collection('users');
-        let result = await col.find({}).toArray();
-        res.send({
-            users: result
-        });
-    } catch (err) {
-        res.send({
-            error: err
-        });
-    }
-    client.close();
+router.get("/login", async function(req, res, next) {
+  const client = new MongoClient(MONGODB_URI, { useNewUrlParser: true });
+  try {
+    await client.connect();
+    const db = client.db(dbName);
+    const col = db.collection("users");
+    let result = await col.find({}).toArray();
+    res.send({
+      users: result
+    });
+  } catch (err) {
+    res.send({
+      error: err
+    });
+  }
+  client.close();
 });
 
 /* SIGN IN */
-router.post('/login', async function (req, res) {
-    const client = new MongoClient(MONGODB_URI, {useNewUrlParser: true});
-    try {
-        await client.connect();
-        const db = client.db(dbName);
-        const col = db.collection('users');
-        if (!validator.validate(req.body.email)) {
-            res.status(400).send({error: 'Email invalide'});
-        } else if (req.body.password.length < 5) {
-            res.status(400).send({error: 'Le mot de passe doit contenir au moins 5 caractères'});
-        } else {
-            var result = await col.find({email: req.body.email, password: md5(req.body.password)}).toArray();
-            if (result.length) {
-                jwt.sign({
-                    _id: result[0]._id,
-                    email: result[0].email,
-                    username: result[0].username,
-                    sexe: result[0].sexe,
-                    preference: result[0].preference
-                }, JWT_KEY, {expiresIn: '24h'}, (err, token) => {
-                    if (err) {
-                        res.send({message: 'error'});
-                    } else {
-                        res.send({
-                            token,
-                            error: null
-                        });
-                    }
-                });
-            } else {
-                res.status(403).send({
-                    error: 'Cet identifiant ou mot de passe est inconnu'
-                });
-            }
-        }
-    } catch (err) {
-        res.send({
-            error: err
-        })
-    }
-    client.close();
-});
-
-/* POST users */
-router.post('/signup', async function (req, res, next) {
-    const client = new MongoClient(MONGODB_URI, {useNewUrlParser: true});
+router.post("/login", async function(req, res) {
+  const client = new MongoClient(MONGODB_URI, { useNewUrlParser: true });
+  try {
     await client.connect();
     const db = client.db(dbName);
-    const col = db.collection('users');
-    //INSERT ONE DOCUMENT
-    let data = await col.find({}).toArray();
+    const col = db.collection("users");
     if (!validator.validate(req.body.email)) {
-        res.status(400).send({error: 'Email invalide'});
-    } else if(!isUsernameValid(req.body.username)){
-        res.status(400).send({error: 'Le nom d\'utilisateur ne doit contenir uniquement des lettres'});
+      res.status(400).send({ error: "Email invalide" });
     } else if (req.body.password.length < 5) {
-        res.status(400).send({error: 'Le mot de passe doit contenir au moins 5 caractères'});
-    } else if (data.some(data => data.email === req.body.email)) {
-        res.status(400).send({error: 'Cet email est déjà associé à un compte'});
+      res
+        .status(400)
+        .send({ error: "Le mot de passe doit contenir au moins 5 caractères" });
     } else {
-        //INSERT ONE DOCUMENT
-        await col.insertOne({
-            email: req.body.email,
-            username: req.body.username,
-            password: md5(req.body.password),
-            photo: req.body.photo,
-            birthdate: req.body.birthdate,
-            sexe: req.body.sexe,
-            preference: req.body.preference,
-            bio: req.body.bio,
-            createdAt: dateNow(),
-            updatedAt: null,
-
-            isDeleted: false,
-            reported: false,
-            banned: false
-
-        });
-        let result = await col.find({username: req.body.username, password: md5(req.body.password)}).toArray();
-        jwt.sign({
+      var result = await col
+        .find({ email: req.body.email, password: md5(req.body.password) })
+        .toArray();
+      if (result.length) {
+        jwt.sign(
+          {
             _id: result[0]._id,
             email: result[0].email,
             username: result[0].username,
             sexe: result[0].sexe,
             preference: result[0].preference
-        }, JWT_KEY, {expiresIn: '24h'}, (err, token) => {
+          },
+          JWT_KEY,
+          { expiresIn: "24h" },
+          (err, token) => {
             if (err) {
-                res.send({message: 'error'});
+              res.send({ message: "error" });
             } else {
-                res.send({
-                    token,
-                    error: null
-                });
+              res.send({
+                token,
+                error: null
+              });
             }
+          }
+        );
+      } else {
+        res.status(403).send({
+          error: "Cet identifiant ou mot de passe est inconnu"
         });
+      }
     }
+  } catch (err) {
+    res.send({
+      error: err
+    });
+  }
+  client.close();
+});
+
+/* POST users */
+router.post("/signup", async function(req, res, next) {
+  const client = new MongoClient(MONGODB_URI, { useNewUrlParser: true });
+  await client.connect();
+  const db = client.db(dbName);
+  const col = db.collection("users");
+  //INSERT ONE DOCUMENT
+  let data = await col.find({}).toArray();
+  if (!validator.validate(req.body.email)) {
+    res.status(400).send({ error: "Email invalide" });
+  } else if (!isUsernameValid(req.body.username)) {
+    res
+      .status(400)
+      .send({
+        error: "Le nom d'utilisateur ne doit contenir uniquement des lettres"
+      });
+  } else if (req.body.password.length < 5) {
+    res
+      .status(400)
+      .send({ error: "Le mot de passe doit contenir au moins 5 caractères" });
+  } else if (data.some(data => data.email === req.body.email)) {
+    res.status(400).send({ error: "Cet email est déjà associé à un compte" });
+  } else {
+    //INSERT ONE DOCUMENT
+    await col.insertOne({
+      email: req.body.email,
+      username: req.body.username,
+      password: md5(req.body.password),
+      photo: req.body.photo,
+      birthdate: req.body.birthdate,
+      sexe: req.body.sexe,
+      preference: req.body.preference,
+      bio: req.body.bio,
+      createdAt: dateNow(),
+      updatedAt: null,
+      participationEvent: null,
+      isDeleted: false,
+      reported: false,
+      banned: false
+    });
+    let result = await col
+      .find({ username: req.body.username, password: md5(req.body.password) })
+      .toArray();
+    jwt.sign(
+      {
+        _id: result[0]._id,
+        email: result[0].email,
+        username: result[0].username,
+        sexe: result[0].sexe,
+        preference: result[0].preference
+      },
+      JWT_KEY,
+      { expiresIn: "24h" },
+      (err, token) => {
+        if (err) {
+          res.send({ message: "error" });
+        } else {
+          res.send({
+            token,
+            error: null
+          });
+        }
+      }
+    );
+  }
 });
 
 module.exports = router;
