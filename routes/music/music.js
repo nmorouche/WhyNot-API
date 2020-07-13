5cd9bce52aa3ec04c0c7fac22b10f593392db602
Ramzy Kmd <gkermad.ramzy@gmail.com>
2020-07-13 18:59:47 +0200

    maj

--- routes/music/music.js
+++ routes/music/music.js
@@ -6,9 +6,36 @@ const { BASEAPPURL } = require("../../config");
 router.get("/", async (req, res, next) => {
   try {
     let musicList = [
-      BASEAPPURL + "public/music/6ix9ine-gooba.mp3",
-      BASEAPPURL + "public/music/dababy-bop.mp3",
-      BASEAPPURL + "public/music/roddy-ricch-the-box.mp3",
+      {
+        title: "La vie qu'on mène",
+        artist: "Ninho",
+        preview:
+          "https://cdns-preview-6.dzcdn.net/stream/c-6c9e8655cb83ae2b5f08ff03ba36a4f2-5.mp3",
+      },
+      {
+        title: "Clarinet Quintet in A Major, K. 581: II. Larghetto",
+        artist: "Mozart",
+        preview:
+          "https://cdns-preview-f.dzcdn.net/stream/c-fd79f95a1e0fa4e44381a6504affcd1f-2.mp3",
+      },
+      {
+        title: "Cassage de nuques, pt. 3",
+        artist: "JUJUJUJUL",
+        preview:
+          "https://cdns-preview-1.dzcdn.net/stream/c-17b598dd082a95bf266fbae928dd65f5-5.mp3",
+      },
+      {
+        title: "CC'est mort",
+        artist: "PLK",
+        preview:
+          "https://cdns-preview-6.dzcdn.net/stream/c-642aeb13e2c03dedd38cad0bf94c56df-3.mp3",
+      },
+      {
+        title: "PUFF PUFF PUFF",
+        artist: "Gambi",
+        preview:
+          "https://cdns-preview-3.dzcdn.net/stream/c-3ca6ee5af1e6cb101b5f062b21831361-4.mp3",
+      },
     ];
 
     res.status(200).json({
